6cb7d4da2a2c0b734d8437ff6f0ae29410a24100
hamzafarooq <hamza50@gmail.com>
2020-10-30 16:27:01 -0700

    commit to code

--- knn/knn.py
+++ knn/knn.py
@@ -11,13 +11,21 @@ class knn:
 
     def fit(self,X,y):
         self.X_train = X
-        self.Y = Y
+        self.y_train = y
 
     def predict(self,X):
         y_pred = [self._predict(x) for x in X]
         return np.array(y_pred)
 
-    def _predict(self,x)
+    def _predict(self,x):
+        distances = [euclidean(x, x_train) for x_train in self.X_train]
+        # Sort by distance and return indices of the first k neighbors
+        k_idx = np.argsort(distances)[:self.k]
+        # Extract the labels of the k nearest neighbor training samples
+        k_neighbor_labels = [self.y_train[i] for i in k_idx]  
+        # return the most common class label
+        most_common = Counter(k_neighbor_labels).most_common(1)
+        return most_common[0][0]
 
 
 
